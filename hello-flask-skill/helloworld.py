10562c15485bc4e7d39c84c23edb719e092e17fb
dawong <darlene.py@gmail.com>
2018-08-13 22:45:09 -0700

    Remove unused imports.  Removed usage of reprompts and cards.  Leave ASK_VERIFY_REQUESTS set to the default for this example.

--- hello-flask-skill/helloworld.py
+++ hello-flask-skill/helloworld.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from flask import Flask
-from flask_ask import Ask, request, session, question, statement
+from flask_ask import Ask, question, statement
 
 
 app = Flask(__name__)
@@ -13,20 +13,20 @@ logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 @ask.launch
 def launch():
     speech_text = 'Welcome to Hello World, you can say hello'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    return question(speech_text)
 
 
 @ask.intent('HelloWorldIntent')
 def hello_world(name=None):
     name = "world" if name is None else name
     speech_text = f'Hello {name}'
-    return question(speech_text).simple_card('HelloWorld', speech_text)
+    return question(speech_text)
 
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
     speech_text = 'You can say hello to me, or ask me to say hello to someone!'
-    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+    return question(speech_text)
 
 
 @ask.intent('AMAZON.StopIntent')
@@ -41,8 +41,4 @@ def session_ended():
 
 
 if __name__ == '__main__':
-    if 'ASK_VERIFY_REQUESTS' in os.environ:
-        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
-        if verify == 'false':
-            app.config['ASK_VERIFY_REQUESTS'] = False
     app.run(debug=True)
